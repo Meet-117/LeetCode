288528201f21ca8995b4cb0c42d2255530e6f448
Meet-117 <meetkp1172@gmail.com>
2025-07-23 22:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (65.76%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) {
+            return false;
+        }
+        int n = matrix[0].length;
+        if (n == 0) {
+            return false;
+        }
+        int begin = 0;
+        int end = m - 1;
+        int rope = 0;
+        int found = -1;
+        while (begin <= end) {
+            rope = begin + (end - begin) / 2;
+            if (matrix[rope][0] == target) {
+                return true;
+            }
+            if (matrix[rope][0] < target) {
+                if (rope == m - 1 || target < matrix[rope + 1][0]) {
+                    found = rope;
+                    break;
+                }
+                begin = rope + 1;
+            } else {
+                end = rope - 1;
+            }
+        }
+        if (found == -1) {
+            return false;
+        }
+        int left = 0;
+        int right = matrix[found].length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (matrix[found][mid] == target) {
+                return true;
+            } else if (matrix[found][mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
