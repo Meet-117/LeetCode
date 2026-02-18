be6d0bcd26b02a6bfc2671a7b6f13a7011e686d8
Patel Meet Kuntalkumar <23cp054@bvmengineering.ac.in>
2026-02-18 15:24:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.84%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if (m == 0) {
-            nums1 = nums2;
-            return;
-        }
-        if (n == 0) {
-            return;
-        }
-        vector<int> v(m + n);
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while(j < m && k < n)
+        int i = m + n - 1;
+        int j = m - 1;
+        int k = n - 1;
+
+        while(j >= 0 && k >= 0)
         {
             if(nums1[j] > nums2[k])
             {
-                v[i++] = nums2[k++];
+                nums1[i--] = nums1[j--];
             }
-            else if(nums1[j] < nums2[k])
+            else
             {
-                v[i++] = nums1[j++];
-            }
-            else if(nums1[j] == nums2[k])
-            {
-                v[i++] = nums1[j++];
-                v[i++] = nums2[k++];
+                nums1[i--] = nums2[k--];
             }
         }
-        while(j < m)
+        while(k >= 0)
         {
-            v[i++] = nums1[j++];
+            nums1[i--] = nums2[k--];
         }
-        while(k < n)
-        {
-            v[i++] = nums2[k++];
-        }
-        nums1 = v;
     }
 };
