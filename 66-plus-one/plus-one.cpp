810bfc074c7e398df4fbe4df5f35cdfee9df0b87
Patel Meet Kuntalkumar <23cp054@bvmengineering.ac.in>
2026-02-18 14:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (53.33%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(), digits.end());
+        digits[0]++;
+        for(int i = 0; i < digits.size(); i++)
+        {
+            if(digits[i] == 10)
+            {
+                digits[i] = 0;
+                if(i == digits.size() - 1)
+                {
+                    digits.push_back(1);
+                }
+                else
+                {
+                    digits[i + 1] += 1;
+                }
+            }
+            else break;
+        }
+        if(digits[digits.size() - 1] == 10)
+        {
+            digits[digits.size() - 1] = 0;
+            digits.push_back(1);
+        }
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
