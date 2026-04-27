8ed06ef1a8e96811db4136bc2baf80d81091cde1
Patel Meet Kuntalkumar <23cp054@bvmengineering.ac.in>
2026-04-27 21:59:54 +0530

    Time: 4 ms (10.39%) | Memory: 23.9 MB (54.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            nums[j] = nums[i];
+            j++;
+        }
+        while (j < nums.size()) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
