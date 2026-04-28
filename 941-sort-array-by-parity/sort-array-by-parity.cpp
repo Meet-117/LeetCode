9c7e6bf245bde70509aa336d1a158a42cbd32057
Patel Meet Kuntalkumar <23cp054@bvmengineering.ac.in>
2026-04-28 20:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (77.99%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+        return nums;
+    }
+};
