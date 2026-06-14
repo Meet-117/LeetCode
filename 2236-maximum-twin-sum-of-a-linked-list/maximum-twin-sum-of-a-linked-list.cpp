95c97dcae0a8a8248ec512b4bfdb7e458c95fc54
Patel Meet Kuntalkumar <23cp054@bvmengineering.ac.in>
2026-06-14 09:20:48 +0530

    Time: 2 ms (70.05%) | Memory: 124.3 MB (67.45%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,29 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        int maxVal = INT_MIN;
-
-        vector<int> nums;
-
-        while (head != nullptr) {
-            nums.push_back(head->val);
-            head = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int n = nums.size();
-        for (auto i{0uz}; i < n / 2; i++) {
-            maxVal = max(maxVal, nums[i] + nums[n - i - 1]);
+
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* nxt = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nxt;
         }
-        return maxVal;
+
+        int ans = 0;
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            ans = max(ans, first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
+
+        return ans;
     }
 };
